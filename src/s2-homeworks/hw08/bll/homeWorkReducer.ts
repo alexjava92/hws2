41ede1fc84b501cb1f105a44cc829b7ccf22e792
alexjava92 <autoexpert37@yandex.ru>
2023-12-23 10:01:30 +0300

    sprint2 HW 08

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,15 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            let stateCopy = state.map(el => el)
+            return action.payload === 'up' ? stateCopy.sort((a, b) => a.name.localeCompare(b.name)) : stateCopy.sort((a, b) => b.name.localeCompare(a.name))
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(el => el.age >= 18)
         }
         default:
             return state
